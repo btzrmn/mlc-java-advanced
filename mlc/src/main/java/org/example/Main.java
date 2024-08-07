dc9f1cc07b32048719150c3cc8fa438937be70b9
batzorig <batzorig.b@nomadicss.mn>
2024-08-07 20:06:44 +0800

    added lesson 10 labs

--- mlc/src/main/java/org/example/Main.java
+++ mlc/src/main/java/org/example/Main.java
@@ -1,39 +1,38 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] elements = reader.readLine().substring(10).split(", ");
-        int[] universe = new int[elements.length];
-        for (int i = 0; i < elements.length; i++) {
-            universe[i] = Integer.parseInt(elements[i]);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] command1 = scanner.nextLine().split(": ");
+        int[] coins = Arrays.stream(command1[1].split(", "))
+                .mapToInt(Integer::parseInt).toArray();
+        String[] command2 = scanner.nextLine().split(": ");
+        int sum = Integer.parseInt(command2[1]);
+        Map<Integer, Integer> map = chooseCoins(coins, sum);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.printf("%d -> %d%n", entry.getKey(), entry.getValue());
         }
-        int numberOfSets = Integer.parseInt(reader.readLine().substring(16));
-        List<int[]> sets = new ArrayList<>();
-        for (int i = 0; i < numberOfSets; i++) {
-            String[] setElements = reader.readLine().split(", ");
-            int[] set = new int[setElements.length];
-            for (int j = 0; j < setElements.length; j++) {
-                set[j] = Integer.parseInt(setElements[j]);
-            }
-            sets.add(set);
-        }
-        List<int[]> chosenSets = chooseSets(sets, universe);
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.format("Sets to take (%d):%n", chosenSets.size()));
-        for (int[] set : chosenSets) {
-            sb.append("{ ");
-            sb.append(Arrays.toString(set).replaceAll("\\[|]", ""));
-            sb.append(" }").append(System.lineSeparator());
-        }
-        System.out.println(sb);
     }
 
+    public static Map<Integer, Integer> chooseCoins(int[] coins, int sum) {
+        List<Integer> listCoins = Arrays.stream(coins).boxed()
+                .sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        int currentSum = 0;
+        int coinIndex = 0;
+        while (currentSum != sum && coinIndex < listCoins.size()) {
+            int currentCoin = listCoins.get(coinIndex);
+            int remainder = sum - currentSum;
+            int sizeOfCoin = remainder / currentCoin;
+            if (currentSum + currentCoin <= sum) {
+                map.put(currentCoin, sizeOfCoin);
+                currentSum = currentSum + sizeOfCoin * currentCoin;
+            }
+            coinIndex++;
+        }
+        return map;
+    }
 }
