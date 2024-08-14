2115eddc46f6d1d671b434e078b2f45200f2fc4e
batzorig <batzorig.b@nomadicss.mn>
2024-08-14 20:57:40 +0800

    added exam preperation 1

---
+++ Exam-Preperation/Exam Preparation - 1/vetClinic/Main.java
@@ -0,0 +1,6 @@
+package vetClinic;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
--- mlc/src/main/java/org/example/Main.java
+++ mlc/src/main/java/org/example/Main.java
@@ -2,5 +2,30 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Clinic clinic = new Clinic(20);
+        Pet dog = new Pet("Ellias", 5, "Tim");
+        System.out.println(dog);
+        clinic.add(dog);
+
+        System.out.println(clinic.remove("Ellias")); // true
+        System.out.println(clinic.remove("Pufa")); // false
+
+        Pet cat = new Pet("Bella", 2, "Mia");
+        Pet bunny = new Pet("Zak", 4, "Jon");
+
+        clinic.add(cat);
+        clinic.add(bunny);
+
+        Pet oldestPet = clinic.getOldestPet();
+        System.out.println(oldestPet); // Zak 4 (Jon)
+
+        Pet pet = clinic.getPet("Bella", "Mia");
+        System.out.println(pet); // Bella 2 (Mia)
+
+        System.out.println(clinic.getCount()); // 2
+
+        System.out.println(clinic.getStatistics());
+
+
     }
 }
