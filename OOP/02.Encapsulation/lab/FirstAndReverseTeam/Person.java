0ad4c28a4ce26be2078d66de9152e02e5006c795
G.Gan-Erdene <ganerdene.g@novelsoft.mn>
2024-09-01 19:02:16 +0800

    add encapsulation lab

---
+++ OOP/02.Encapsulation/lab/FirstAndReverseTeam/Person.java
@@ -0,0 +1,75 @@
+package oop01.FirstAndReverseTeam;
+
+import java.text.DecimalFormat;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAge(age);
+        this.setSalary(salary);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName.length() < 3) {
+            throw new IllegalArgumentException("First name cannot be less than 3 symbols");
+        }
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.length() < 3) {
+            throw new IllegalArgumentException("Last name cannot be less than 3 symbols");
+        }
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (age < 1) {
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 460) {
+            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+        }
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage) {
+        if (this.age < 30) {
+            this.setSalary(this.salary + (this.salary * percentage / 200));
+        } else {
+            this.setSalary(this.salary + (this.salary * percentage / 100));
+        }
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat formatter = new DecimalFormat("#.0####");
+        return String.format("%s %s gets %s leva", this.getFirstName(), this.getLastName(), formatter.format(this.getSalary()));
+    }
+}
---
+++ OOP/02.Encapsulation/lab/SalaryIncrease/Person.java
@@ -0,0 +1,51 @@
+package oop01.SalaryIncrease;
+
+import java.text.DecimalFormat;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage) {
+        if (this.age < 30) {
+            this.setSalary(this.salary + (this.salary * percentage / 200));
+        } else {
+            this.setSalary(this.salary + (this.salary * percentage / 100));
+        }
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat formatter = new DecimalFormat("#.0####");
+        return String.format("%s %s gets %s USD.", this.getFirstName(), this.getLastName(), formatter.format(this.getSalary()));
+    }
+}
